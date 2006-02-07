baf1415e2ab80cfa244cd3890a92c4637b8c1180
lvonk <lvonk@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2006-02-07 20:50:24 +0000

    Added javadoc.

--- branches/trails-spring-mvc/src/org/trails/spring/mvc/ChildObjectDataDescriptor.java
+++ branches/trails-spring-mvc/src/org/trails/spring/mvc/ChildObjectDataDescriptor.java
@@ -1,6 +1,17 @@
 package org.trails.spring.mvc;
 
+/**
+ * Class is used for Objects that are values of in collection of a parent object
+ * in a {@link org.trails.spring.mvc.ObjectDataDescriptorList}. For this situation
+ * we only need the id of the class and we don't want to populate the entire
+ * ObjectDataDescriptor with
+ * {@link org.trails.spring.mvc.ObjectDataDescriptor#getColumns() colums} etc.
+ * 
+ * @author Lars Vonk
+ *
+ */
 public class ChildObjectDataDescriptor extends ObjectDataDescriptor {
+  
   /** Holds the id of the instance in the ObjectDataDescriptor. */
   private Object id = null;
 
