67e54b5add7c73878cb79174e5e56fcf4df3ec46
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2007-10-01 01:05:23 +0000

    [maven-release-plugin]  copy for tag trails-1.1

---
+++ tags/trails-1.1/examples/simple/src/test/java/org/trails/test/functional/TableBlockOverrideTest.java
@@ -0,0 +1,19 @@
+package org.trails.test.functional;
+
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class TableBlockOverrideTest extends FunctionalTest
+{
+	public void testThatBlockOverrideAppears() throws Exception
+	{
+		HtmlPage listThingsPage = clickLinkOnPage(startPage, "List Things");
+		HtmlPage newThingPage = clickLinkOnPage(listThingsPage, "New Thing");
+		HtmlForm newThingForm = getFirstForm(newThingPage);
+		getInputByName(newThingPage, "Name").setValueAttribute("blah");
+		getInputByName(newThingPage, "Id").setValueAttribute("1");
+		listThingsPage = clickButton(newThingForm, "Ok");
+		assertXPathPresent(listThingsPage, "//p[text()='This is where the name should go.blah']");
+
+	}
+}
