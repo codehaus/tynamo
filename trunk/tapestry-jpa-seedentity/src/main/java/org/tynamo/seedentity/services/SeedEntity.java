92692128a2148086fd3b7a4bc1c6b83e2ef4a279
psartini <psartini@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2010-01-19 03:04:43 +0000

    added tapestry-jpa-seedentity module

---
+++ trunk/tapestry-jpa-seedentity/src/main/java/org/tynamo/seedentity/services/SeedEntity.java
@@ -0,0 +1,5 @@
+package org.tynamo.seedentity.services;
+
+public interface SeedEntity {
+
+}
---
+++ trunk/tapestry-jpa-seedentity/src/main/java/org/tynamo/seedentity/tools/BeanUtil.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © Pierangelo Sartini, 2010.
+ */
+
+package org.tynamo.seedentity.tools;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Utility class for reading and setting bean properties.
+ * It uses getters and setters and does work with active
+ * security manager.
+ *
+ * @author Pierangelo Sartini
+ */
+public class BeanUtil {
+	public static Object getProperty(Object bean, String name) {
+		Object ret = null;
+		Class clazz = bean.getClass();
+
+		name = name.substring(0, 1).toUpperCase().concat(name.substring(1));
+		try {
+			Method m = null;
+			try {
+				m = clazz.getDeclaredMethod("get" + name);
+			} catch (NoSuchMethodException ex) {
+				m = clazz.getDeclaredMethod("is" + name);
+			}
+			ret = m.invoke(bean);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	public static void setProperty(Object bean, String name, Object value) {
+		Class clazz = bean.getClass();
+
+		name = name.substring(0, 1).toUpperCase().concat(name.substring(1));
+		try {
+			Method m = null;
+			m = clazz.getDeclaredMethod("set" + name, value.getClass());
+			m.invoke(bean, value);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+}
