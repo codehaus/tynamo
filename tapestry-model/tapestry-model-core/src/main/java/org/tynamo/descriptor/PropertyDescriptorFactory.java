278507831d1e91c03f3fddaad8d85f1a6dff408a
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2010-01-25 18:15:01 +0000

    Prepare branch for tapestry-jpa 1.x

--- tapestry-model/tapestry-model-core/src/main/java/org/tynamo/descriptor/PropertyDescriptorFactory.java
+++
@@ -1,9 +0,0 @@
-package org.tynamo.descriptor;
-
-import java.util.ArrayList;
-import java.beans.BeanInfo;
-
-public interface PropertyDescriptorFactory
-{
-	ArrayList<TynamoPropertyDescriptor> buildPropertyDescriptors(Class beanType, BeanInfo beanInfo) throws Exception;
-}
