a669782e52a1652011a66746d9a170265c952b2a
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2010-10-06 20:50:36 +0000

    [maven-release-plugin]  copy for branch tapestry-security-0.2.x

---
+++ branches/tapestry-security-0.2.x/src/main/java/org/tynamo/security/SecuritySymbols.java
@@ -0,0 +1,14 @@
+package org.tynamo.security;
+
+public class SecuritySymbols
+{
+
+	public static final String LOGIN_URL = "security.loginurl";
+	public static final String SUCCESS_URL = "security.successurl";
+	public static final String UNAUTHORIZED_URL = "security.unauthorizedurl";
+
+	public static final String DEFAULTSIGNINPAGE = "security.defaultsigninpage";
+
+	public static final String CONFIG_PATH = "security.configpath";
+	public static final String SHOULD_LOAD_INI_FROM_CONFIG_PATH = "security.loadinifrompath";
+}
