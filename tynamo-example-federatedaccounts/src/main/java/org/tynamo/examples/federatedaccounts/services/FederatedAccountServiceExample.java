b751bd2a2fe34963479dd800fd1dbd3db6301ee4
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-01-11 22:33:42 +0000

    Initial import of a GAE runnable federatedaccounts example

---
+++ tynamo-example-federatedaccounts/src/main/java/org/tynamo/examples/federatedaccounts/services/FederatedAccountServiceExample.java
@@ -0,0 +1,24 @@
+package org.tynamo.examples.federatedaccounts.services;
+
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.tapestry5.services.ApplicationStateManager;
+import org.tynamo.examples.federatedaccounts.session.CurrentUser;
+import org.tynamo.security.federatedaccounts.services.FederatedAccountService;
+
+public class FederatedAccountServiceExample implements FederatedAccountService {
+	private final ApplicationStateManager applicationStateManager;
+
+	public FederatedAccountServiceExample(ApplicationStateManager applicationStateManager) {
+		this.applicationStateManager = applicationStateManager;
+	}
+
+	@Override
+	public AuthenticationInfo federate(String realmName, Object remotePrincipal, AuthenticationToken authenticationToken, Object remoteAccount) {
+		CurrentUser currentUser = applicationStateManager.get(CurrentUser.class);
+		currentUser.merge(remoteAccount);
+		return new SimpleAuthenticationInfo(currentUser.getUsername(), authenticationToken.getCredentials(), realmName);
+	}
+
+}
