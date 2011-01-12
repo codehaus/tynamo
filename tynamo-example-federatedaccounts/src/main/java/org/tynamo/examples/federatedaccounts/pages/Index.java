0edf9eb078a98b7354a74bd05a96ba1a676b3e6d
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-01-12 17:36:11 +0000

    Add a logout action

--- tynamo-example-federatedaccounts/src/main/java/org/tynamo/examples/federatedaccounts/pages/Index.java
+++ tynamo-example-federatedaccounts/src/main/java/org/tynamo/examples/federatedaccounts/pages/Index.java
@@ -18,11 +18,13 @@
  */
 package org.tynamo.examples.federatedaccounts.pages;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.ApplicationStateManager;
 import org.apache.tapestry5.services.ExceptionReporter;
+import org.apache.tapestry5.services.Request;
 import org.tynamo.examples.federatedaccounts.session.CurrentUser;
 import org.tynamo.security.services.SecurityService;
 
@@ -64,5 +66,23 @@ public class Index implements ExceptionReporter {
 			currentUser.merge(securityService.getSubject().getPrincipal());
 		}
 
+	}
+
+	@Inject
+	private Request request;
+
+	Object onActionFromLogout() {
+		// Need to call this explicitly to invoke onlogout handlers (for remember me etc.)
+		SecurityUtils.getSubject().logout();
+		try {
+			// the session is already invalidated, but need to cause an exception since tapestry doesn't know about it
+			// and you'll get a container exception message instead without this. Unfortunately, there's no way of
+			// configuring Shiro to not invalidate sessions right now. See DefaultSecurityManager.logout()
+			// There's a similar issues in Tapestry - Howard has fixed, but no in T5.2.x releases yet
+			request.getSession(false).invalidate();
+		} catch (Exception e) {
+		}
+
+		return this;
 	}
 }
