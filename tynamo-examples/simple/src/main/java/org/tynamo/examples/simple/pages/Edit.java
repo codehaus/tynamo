4eff40c559a4c9c151d2039fb0885b46afa35a00
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-01-13 20:14:28 +0000

    [maven-release-plugin]  copy for tag esxx-httpclient-gae-0.0.1

--- tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/pages/Edit.java
+++
@@ -1,6 +0,0 @@
-package org.tynamo.examples.simple.pages;
-
-
-public class Edit extends org.tynamo.pages.Edit {
-
-}
--- tynamo-federatedaccounts/src/main/java/org/tynamo/security/federatedaccounts/HostSymbols.java
+++
@@ -1,9 +0,0 @@
-package org.tynamo.security.federatedaccounts;
-
-public class HostSymbols {
-	public static final String HOSTNAME = "hostname";
-
-	public static final String COMMITAFTER_OAUTH = "federatedaccounts.commitafteroauth";
-
-	public static final String HTTPCLIENT_ON_GAE = "httpclient.gae";
-}
