b9d5f9b6fd831fbfad3fdb53f958d1a9cd9c7bf4
ascandroli <ascandroli@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-01-21 22:56:00 +0000

    applying patch for TYNAMO-71 provided by Pierce T. Wetter III

--- tapestry-jpa-module/tapestry-jpa/src/main/java/org/tynamo/jpa/internal/JPAGridDataSource.java
+++ tapestry-jpa-module/tapestry-jpa/src/main/java/org/tynamo/jpa/internal/JPAGridDataSource.java
@@ -14,41 +14,61 @@
 
 package org.tynamo.jpa.internal;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Root;
-
 import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.grid.SortConstraint;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.List;
+
+
 /**
- * A simple implementation of {@link org.apache.tapestry5.grid.GridDataSource} based on a Hibernate
- * Session and a known entity class. This implementation does support multiple
- * {@link org.apache.tapestry5.grid.SortConstraint sort constraints}; however it assumes a direct
- * mapping from sort constraint property to Hibernate property.
+ * A simple implementation of {@link org.apache.tapestry5.grid.GridDataSource} based on a
+ * EntityManager Session and a known entity class. This implementation does support multiple {@link
+ * org.apache.tapestry5.grid.SortConstraint sort constraints}; however it assumes a direct mapping
+ * from sort constraint property to Hibernate property.
  * <p/>
- * This class is <em>not</em> thread-safe; it maintains internal state.
+ * <p/>This class is <em>not</em> thread-safe; it maintains internal state.</p>
  * <p/>
- * Typically, an instance of this object is created fresh as needed (that is, it is not stored
- * between requests).
+ * <p>Typically, an instance of this object is created fresh as needed (that is, it is not stored
+ * between requests).</p>
  *
  * @param <E>
  */
 public class JPAGridDataSource<E> implements GridDataSource
 {
+
+	//~ Instance fields ----------------------------------------------------------------------------
+
+	/**
+	 * EntityManager we're doing work for
+	 */
 	protected final EntityManager entityManager;
 
+	/**
+	 * EntityType we're fetching
+	 */
 	private final Class<E> entityType;
 
+	/**
+	 * starting index
+	 */
 	private int startIndex;
 
+	/**
+	 * The prepared results
+	 */
 	private List preparedResults;
 
+	//~ Constructors -------------------------------------------------------------------------------
+
+	/**
+	 * Creates a new JPAGridDataSource object.
+	 *
+	 * @param em         EntityManager to use
+	 * @param entityType Entity to query for
+	 */
 	public JPAGridDataSource(EntityManager em, Class<E> entityType)
 	{
 		assert em != null;
@@ -58,9 +78,14 @@ public class JPAGridDataSource<E> implements GridDataSource
 		this.entityType = entityType;
 	}
 
+	//~ Methods ------------------------------------------------------------------------------------
+
 	/**
 	 * Returns the total number of rows for the configured entity type.
+	 *
+	 * @return total number of rows
 	 */
+	@Override
 	public int getAvailableRows()
 	{
 		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
@@ -73,8 +98,7 @@ public class JPAGridDataSource<E> implements GridDataSource
 		if (query != null)
 		{
 			resultQuery.where(query).select(cb.count(all));
-		}
-		else
+		} else
 		{
 			resultQuery.select(cb.count(all));
 		}
@@ -86,19 +110,39 @@ public class JPAGridDataSource<E> implements GridDataSource
 	}
 
 	/**
+	 * Turn a list with things to follow into a Path object for the order by clause
+	 *
+	 * @param all  root to start from
+	 * @param path property name/path
+	 * @return a JPA Criteria API Path object
+	 */
+	public Path buildPath(Root<E> all, String path)
+	{
+		path = path.replaceAll("\\?\\.", ".");
+
+		Path start = all;
+
+		for (String word : path.split("\\."))
+		{
+			start = start.get(word);
+		}
+
+		return start;
+	}
+
+	/**
 	 * Prepares the results, performing a query (applying the sort results, and the provided start
 	 * and end index). The results can later be obtained from {@link #getRowValue(int)} .
 	 *
-	 * @param startIndex
-	 *            index, from zero, of the first item to be retrieved
-	 * @param endIndex
-	 *            index, from zero, of the last item to be retrieved
-	 * @param sortConstraints
-	 *            zero or more constraints used to set the order of the returned values
+	 * @param startIndex      index, from zero, of the first item to be retrieved
+	 * @param endIndex        index, from zero, of the last item to be retrieved
+	 * @param sortConstraints zero or more constraints used to set the order of the returned
+	 *                        values
 	 */
+	@Override
 	public void prepare(int startIndex, int endIndex, List<SortConstraint> sortConstraints)
 	{
-    assert sortConstraints != null;
+		assert sortConstraints != null;
 
 		// We just assume that the property names in the SortContraint match the Hibernate
 		// properties.
@@ -108,13 +152,12 @@ public class JPAGridDataSource<E> implements GridDataSource
 		Root<E> all = resultQuery.from(entityType);
 		Expression<Boolean> query;
 
-		query = additionalConstraints(cb,all);
+		query = additionalConstraints(cb, all);
 
 		if (query != null)
 		{
 			resultQuery.where(query).select(all);
-		}
-		else
+		} else
 		{
 			resultQuery.select(all);
 		}
@@ -129,11 +172,13 @@ public class JPAGridDataSource<E> implements GridDataSource
 
 				case ASCENDING:
 
-					resultQuery.orderBy(cb.asc(all.get(propertyName)));
+					resultQuery.orderBy(cb.asc(buildPath(all, propertyName)));
+
 					break;
 
 				case DESCENDING:
-					resultQuery.orderBy(cb.desc(all.get(propertyName)));
+					resultQuery.orderBy(cb.desc(buildPath(all, propertyName)));
+
 					break;
 
 				default:
@@ -148,13 +193,17 @@ public class JPAGridDataSource<E> implements GridDataSource
 		this.startIndex = startIndex;
 
 		preparedResults = finalQ.getResultList();
-	}
+	} // end method prepare
 
 	/**
 	 * Invoked after the main criteria has been set up (firstResult, maxResults and any sort
 	 * contraints). This gives subclasses a chance to apply additional constraints before the list
 	 * of results is obtained from the criteria. This implementation does nothing and may be
 	 * overridden.
+	 *
+	 * @param cb   CriteriaBuilder to use for building constraints
+	 * @param root Root object to use
+	 * @return A boolean expression to use for filtering
 	 */
 	protected Expression<Boolean> additionalConstraints(CriteriaBuilder cb, Root<E> root)
 	{
@@ -165,10 +214,10 @@ public class JPAGridDataSource<E> implements GridDataSource
 	 * Returns a row value at the given index (which must be within the range defined by the call to
 	 * {@link #prepare(int, int, java.util.List)} ).
 	 *
-	 * @param index
-	 *            of object
+	 * @param index of object
 	 * @return object at that index
 	 */
+	@Override
 	public Object getRowValue(int index)
 	{
 		return preparedResults.get(index - startIndex);
@@ -176,9 +225,12 @@ public class JPAGridDataSource<E> implements GridDataSource
 
 	/**
 	 * Returns the entity type, as provided via the constructor.
+	 *
+	 * @return entity type
 	 */
+	@Override
 	public Class getRowType()
 	{
 		return entityType;
 	}
-}
+} // end class JPAGridDataSource
