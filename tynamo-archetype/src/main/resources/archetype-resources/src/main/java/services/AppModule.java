bc709764710e38dcbdea98b56ab0bca80cd4d472
ascandroli <ascandroli@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-02-16 16:20:35 +0000

    fixes TYNAMO-44 - use "es" instead of "es_AR"

--- tynamo-archetype/src/main/resources/archetype-resources/src/main/java/services/AppModule.java
+++ tynamo-archetype/src/main/resources/archetype-resources/src/main/java/services/AppModule.java
@@ -31,7 +31,7 @@ public class AppModule
 		// locales to just "en" (English). As you add localised message catalogs and other assets,
 		// you can extend this list of locales (it's a comma seperated series of locale names;
 		// the first locale name is the default when there's no reasonable match).
-		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en, es_AR, pt");
+		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en, es");
 
 		// The factory default is true but during the early stages of an application
 		// overriding to false is a good idea. In addition, this is often overridden
