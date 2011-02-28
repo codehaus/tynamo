84523e7b7e4450cd03ddb30940ab9902bd3697cd
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-02-28 03:43:20 +0000

    [maven-release-plugin]  copy for tag tynamo-federatedaccounts-0.0.1

---
+++ tags/tynamo-federatedaccounts-0.0.1/src/main/java/org/tynamo/security/federatedaccounts/pages/FacebookOauth.java
@@ -0,0 +1,13 @@
+package org.tynamo.security.federatedaccounts.pages;
+
+import java.net.MalformedURLException;
+
+import org.tynamo.security.federatedaccounts.base.AbstractFacebookOauthPage;
+
+public class FacebookOauth extends AbstractFacebookOauthPage {
+
+	@Override
+	protected void onActivate(String windowMode) throws MalformedURLException {
+		super.onActivate(windowMode);
+	}
+}
