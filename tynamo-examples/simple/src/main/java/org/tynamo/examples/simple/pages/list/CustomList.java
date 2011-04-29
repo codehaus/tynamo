c358726d82414eb58b00ab9fe071d4bdd45dc397
ascandroli <ascandroli@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-04-29 13:42:30 +0000

    fixes TYNAMO-85

--- tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/pages/list/CustomList.java
+++ tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/pages/list/CustomList.java
@@ -12,4 +12,9 @@ public abstract class CustomList<T> extends org.tynamo.pages.List {
 	final protected void onActivate() throws Exception {
 		super.onActivate(getType());
 	}
+
+	@Override
+	final protected Object[] onPassivate() {
+		return new Object[0];
+	}
 }
