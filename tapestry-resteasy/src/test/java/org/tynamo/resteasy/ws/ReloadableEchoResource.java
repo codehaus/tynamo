a60300b6ef97187b7a0d528b3f0c7dffa8f8f149
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-05-02 19:45:22 +0000

    [maven-release-plugin]  copy for branch tapestry-security-0.3.x

--- tapestry-resteasy/src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResource.java
+++
@@ -1,9 +0,0 @@
-package org.tynamo.resteasy.ws;
-
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
-
-public interface ReloadableEchoResource
-{
-	Response echo(@PathParam("message") String message);
-}
