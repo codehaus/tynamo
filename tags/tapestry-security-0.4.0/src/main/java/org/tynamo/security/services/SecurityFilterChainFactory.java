f9abcd611de51b8f7fbd0aeb832357e87779fd47
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-06-04 05:34:21 +0000

    [maven-release-plugin]  copy for tag tapestry-security-0.4.0

---
+++ tags/tapestry-security-0.4.0/src/main/java/org/tynamo/security/services/SecurityFilterChainFactory.java
@@ -0,0 +1,27 @@
+package org.tynamo.security.services;
+
+import org.tynamo.security.services.impl.SecurityFilterChain;
+import org.tynamo.security.shiro.authc.AnonymousFilter;
+import org.tynamo.security.shiro.authc.BasicHttpAuthenticationFilter;
+import org.tynamo.security.shiro.authc.FormAuthenticationFilter;
+import org.tynamo.security.shiro.authc.UserFilter;
+import org.tynamo.security.shiro.authz.PermissionsAuthorizationFilter;
+import org.tynamo.security.shiro.authz.RolesAuthorizationFilter;
+
+public interface SecurityFilterChainFactory {
+	public SecurityFilterChain.Builder createChain(String path);
+
+	public String getLogicalUrl(Class pageClass);
+	
+	public AnonymousFilter anon();
+
+	public UserFilter user();
+
+	public FormAuthenticationFilter authc();
+
+	public BasicHttpAuthenticationFilter basic();
+	
+	public RolesAuthorizationFilter roles();
+	
+	public PermissionsAuthorizationFilter perms();
+}
