5efe602b38a8ef9d93ad8f2b6bf01b774f68f1e5
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-07-14 17:22:17 +0000

    [maven-release-plugin]  copy for branch federatedaccounts-0.0.x

---
+++ branches/federatedaccounts-0.0.x/src/main/java/org/tynamo/security/federatedaccounts/oauth/tokens/OauthAuthenticationToken.java
@@ -0,0 +1,48 @@
+package org.tynamo.security.federatedaccounts.oauth.tokens;
+
+import org.apache.shiro.authc.AuthenticationToken;
+import org.scribe.model.Token;
+
+import java.util.Date;
+
+public class OauthAuthenticationToken implements AuthenticationToken {
+
+	private static final long serialVersionUID = 0L;
+	private Token token;
+	private Date expiration;
+
+	public OauthAuthenticationToken(Token accessToken, long expiresInSeconds) {
+		this(accessToken, new Date(System.currentTimeMillis() + expiresInSeconds * 1000L));
+	}
+
+	public OauthAuthenticationToken(Token accessToken, Date expiration) {
+		this.token = accessToken;
+		this.expiration = expiration;
+	}
+
+	public Token getToken() {
+		return token;
+	}
+
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	public String toString() {
+		return token.getToken();
+	}
+
+	/**
+	 * @return the granted oauth access token
+	 */
+	@Override
+	public Object getPrincipal() {
+		return getToken();
+	}
+
+	@Override
+	public Object getCredentials() {
+		return getExpiration();
+	}
+
+}
