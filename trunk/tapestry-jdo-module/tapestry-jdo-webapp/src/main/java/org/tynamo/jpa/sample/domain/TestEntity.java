11d15cc293c4da5492465af5778bbf8950199a76
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2011-08-29 17:35:15 +0000

    Remote @author tags

--- trunk/tapestry-jdo-module/tapestry-jdo-webapp/src/main/java/org/tynamo/jpa/sample/domain/TestEntity.java
+++ trunk/tapestry-jdo-module/tapestry-jdo-webapp/src/main/java/org/tynamo/jpa/sample/domain/TestEntity.java
@@ -4,9 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-/**
- * @author Piero Sartini
- */
 @Entity
 public class TestEntity {
 	@Id @GeneratedValue
