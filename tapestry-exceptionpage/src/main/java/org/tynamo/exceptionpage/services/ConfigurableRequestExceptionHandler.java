3aa27c5bd05bc91c70639b8d38acb447afd44a96
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-01-16 18:14:43 +0000

    FIXED - issue TYNAMO-114: Improve exception cause handling and documentation

--- tapestry-exceptionpage/src/main/java/org/tynamo/exceptionpage/services/ConfigurableRequestExceptionHandler.java
+++ tapestry-exceptionpage/src/main/java/org/tynamo/exceptionpage/services/ConfigurableRequestExceptionHandler.java
@@ -43,10 +43,23 @@ public class ConfigurableRequestExceptionHandler implements RequestExceptionHand
 		this.exceptionHandler = exceptionHandler;
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	protected Object[] formExceptionContext(Throwable exception) {
 		if (exception instanceof ContextAwareException) return ((ContextAwareException) exception).getContext();
-		if (exception.getMessage() == null) return new Object[0];
-		return new Object[] { exception.getMessage() };
+
+		Class exceptionClass = exception.getClass();
+		// pick the first class in the hierarchy that's not anonymous, probably no reason check for array types
+		while ("".equals(exceptionClass.getSimpleName()))
+			exceptionClass = exceptionClass.getSuperclass();
+
+		// check if exception type is plain runtimeException - yes, we really want the test to be this way
+		if (exceptionClass.isAssignableFrom(RuntimeException.class))
+			return exception.getMessage() == null ? new Object[0] : new Object[] { exception.getMessage().toLowerCase() };
+
+		// otherwise, form the context from the exception type name
+		String exceptionType = exceptionClass.getSimpleName();
+		if (exceptionType.endsWith("Exception")) exceptionType = exceptionType.substring(0, exceptionType.length() - 9);
+		return new Object[] { exceptionType.toLowerCase() };
 	}
 
 	public void handleRequestException(Throwable exception) throws IOException {
