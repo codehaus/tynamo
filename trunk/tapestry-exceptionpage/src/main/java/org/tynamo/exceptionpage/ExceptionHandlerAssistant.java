4046ca959b886a2c0ef2821c329be01db56f0a32
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-01-19 17:40:25 +0000

    Allow returning any object from ExceptionHandlerAssistant's handle operation

--- trunk/tapestry-exceptionpage/src/main/java/org/tynamo/exceptionpage/ExceptionHandlerAssistant.java
+++ trunk/tapestry-exceptionpage/src/main/java/org/tynamo/exceptionpage/ExceptionHandlerAssistant.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 import java.util.List;
 
 public interface ExceptionHandlerAssistant {
-	public String handleRequestException(Throwable exception, List<Object> exceptionContext) throws IOException;
+	public Object handleRequestException(Throwable exception, List<Object> exceptionContext) throws IOException;
 }
