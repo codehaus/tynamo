b1d97d0e33ce949d75081619c11f439b70e8d208
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-01-24 22:27:55 +0000

    [maven-release-plugin]  copy for tag tapestry-security-0.4.1

---
+++ tags/tapestry-security-0.4.1/src/main/java/org/tynamo/security/services/PageService.java
@@ -0,0 +1,24 @@
+package org.tynamo.security.services;
+
+import java.io.IOException;
+
+// Deprecated with an intention of renaming this service and making it internal 
+// I don't like the name of the service, not convinced this is needed externally to the module (kaosko 2012-01-15) 
+@Deprecated
+public interface PageService
+{
+
+	String getLoginPage();
+
+	String getSuccessPage();
+
+	String getUnauthorizedPage();
+
+  public String getLocalelessPathWithinApplication();
+  
+	public String getLocaleFromPath(String path);
+	
+  public void saveRequest();
+  
+  public void redirectToSavedRequest(String fallbackUrl) throws IOException;
+}
---
+++ tags/tapestry-security-0.4.1/src/main/java/org/tynamo/security/services/SecurityFilterChainFactory.java
@@ -0,0 +1,31 @@
+package org.tynamo.security.services;
+
+import org.tynamo.security.services.impl.SecurityFilterChain;
+import org.tynamo.security.shiro.authc.AnonymousFilter;
+import org.tynamo.security.shiro.authc.BasicHttpAuthenticationFilter;
+import org.tynamo.security.shiro.authc.FormAuthenticationFilter;
+import org.tynamo.security.shiro.authc.UserFilter;
+import org.tynamo.security.shiro.authz.PermissionsAuthorizationFilter;
+import org.tynamo.security.shiro.authz.PortFilter;
+import org.tynamo.security.shiro.authz.RolesAuthorizationFilter;
+import org.tynamo.security.shiro.authz.SslFilter;
+
+public interface SecurityFilterChainFactory {
+	public SecurityFilterChain.Builder createChain(String path);
+
+	public AnonymousFilter anon();
+
+	public UserFilter user();
+
+	public FormAuthenticationFilter authc();
+
+	public BasicHttpAuthenticationFilter basic();
+	
+	public RolesAuthorizationFilter roles();
+	
+	public PermissionsAuthorizationFilter perms();
+
+	SslFilter ssl();
+
+	PortFilter port();
+}
