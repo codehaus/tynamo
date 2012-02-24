bce118ec36aade2f0c60e11186554a13eff97277
ascandroli <ascandroli@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-02-24 00:14:03 +0000

    fixing the "simple" example

--- tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/entities/CarPk.java
+++ tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/entities/CarPk.java
@@ -3,8 +3,6 @@ package org.tynamo.examples.simple.entities;
 import org.tynamo.PageType;
 import org.tynamo.descriptor.annotation.beaneditor.BeanModel;
 import org.tynamo.descriptor.annotation.beaneditor.BeanModels;
-import org.tynamo.descriptor.annotation.extension.InitialValue;
-import org.tynamo.descriptor.annotation.extension.PossibleValues;
 
 import javax.persistence.Embeddable;
 import javax.persistence.ManyToOne;
@@ -37,7 +35,7 @@ public class CarPk implements Serializable
 	private Model model;
 
 	@Transient
-	@InitialValue("model.make")
+//	#todo @InitialValue("model.make")
 	public Make getMake()
 	{
 		return make;
@@ -50,7 +48,7 @@ public class CarPk implements Serializable
 
 	@NotNull
 	@ManyToOne(optional = false)
-	@PossibleValues("make.models")
+//	#todo @PossibleValues("make.models")
 	public Model getModel()
 	{
 		return model;
--- tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/pages/show/CatalogShow.java
+++ tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/pages/show/CatalogShow.java
@@ -1,31 +1,27 @@
-package org.tynamo.examples.simple.pages;
-
+package org.tynamo.examples.simple.pages.show;
 
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.Link;
 import org.apache.tapestry5.annotations.CleanupRender;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.ContextValueEncoder;
 import org.apache.tapestry5.services.PageRenderLinkSource;
-import org.tynamo.util.TynamoMessages;
+import org.tynamo.descriptor.CollectionDescriptor;
+import org.tynamo.examples.simple.entities.Catalog;
+import org.tynamo.examples.simple.pages.List;
 import org.tynamo.routing.annotations.At;
 import org.tynamo.services.DescriptorService;
 import org.tynamo.services.PersistenceService;
+import org.tynamo.util.TynamoMessages;
 import org.tynamo.util.Utils;
 
-/**
- * Page for displayig the properties of an object.
- *
- * @note:
- * When extending this page for customization purposes, it's better to copy & paste code than trying to use inheritance.
- *
- */
-@At("/{0}/{1}")
-public class Show
+@At(value = "/catalog/{0}", order = "before:Show")
+public class CatalogShow
 {
 
 	@Inject
@@ -44,30 +40,29 @@ public class Show
 	private PageRenderLinkSource pageRenderLinkSource;
 
 	@Property(write = false)
-	private Class beanType;
+	private Class<Catalog> beanType = Catalog.class;
 
 	@Property
-	private Object bean;
+	private Catalog bean;
 
 	@OnEvent(EventConstants.ACTIVATE)
-	Object activate(Class clazz, String id)
+	Object activate(Catalog catalog)
 	{
-
-		if (clazz == null) return Utils.new404(messages);
-
-		this.bean = contextValueEncoder.toValue(clazz, id);
-		this.beanType = clazz;
-
+		this.bean = catalog;
+//		this.bean = contextValueEncoder.toValue(clazz, id);
 		if (bean == null) return Utils.new404(messages);
-
 		return null;
+	}
+
+	public CollectionDescriptor getCategoriesDescriptor()
+	{
+		return (CollectionDescriptor) descriptorService.getClassDescriptor(beanType).getPropertyDescriptor("categories");
 	}
 
 	@CleanupRender
 	void cleanup()
 	{
 		bean = null;
-		beanType = null;
 	}
 
 	/**
@@ -76,7 +71,7 @@ public class Show
 	@OnEvent(EventConstants.PASSIVATE)
 	Object[] passivate()
 	{
-		return new Object[]{beanType, bean};
+		return new Object[]{bean};
 	}
 
 	public Object[] getEditPageContext()
