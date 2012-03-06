ad6fb711257383e88f77a651eac2d02208fef761
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-03-06 23:05:45 +0000

    Incomplete - issue TYNAMO-130: Support localized content - sketch up initial support for locales

--- trunk/tapestry-jpa-editablecontent/src/test/java/org/tynamo/editablecontent/testapp/services/AppModule.java
+++ trunk/tapestry-jpa-editablecontent/src/test/java/org/tynamo/editablecontent/testapp/services/AppModule.java
@@ -37,6 +37,6 @@ public class AppModule {
 		// locales to just "en" (English). As you add localised message catalogs and other assets,
 		// you can extend this list of locales (it's a comma separated series of locale names;
 		// the first locale name is the default when there's no reasonable match).
-		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en, fi_FI");
 	}
 }
