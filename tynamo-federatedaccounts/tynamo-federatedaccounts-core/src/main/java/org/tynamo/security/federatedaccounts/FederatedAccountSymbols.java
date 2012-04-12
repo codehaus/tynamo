6a147a88a1f8d39edf2bc6dda3c7c321ab369fb9
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-04-12 17:15:09 +0000

    [maven-release-plugin]  copy for tag tapestry-model-0.3.0

--- tynamo-federatedaccounts/tynamo-federatedaccounts-core/src/main/java/org/tynamo/security/federatedaccounts/FederatedAccountSymbols.java
+++
@@ -1,9 +0,0 @@
-package org.tynamo.security.federatedaccounts;
-
-public class FederatedAccountSymbols {
-	public static final String SUCCESSURL = "federatedaccounts.successurl";
-
-	public static final String COMMITAFTER_OAUTH = "federatedaccounts.commitafteroauth";
-
-	public static final String HTTPCLIENT_ON_GAE = "httpclient.gae";
-}
