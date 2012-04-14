7c83cefef51a457d28bb6a472ea82cbf6d95f2f8
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-04-14 03:12:42 +0000

    [maven-release-plugin]  copy for tag tynamo-archetype-0.3.0

--- tynamo-federatedaccounts/tynamo-federatedaccounts-core/src/main/java/org/tynamo/security/federatedaccounts/base/AbstractOauthSignIn.java
+++
@@ -1,8 +0,0 @@
-package org.tynamo.security.federatedaccounts.base;
-
-import org.apache.tapestry5.annotations.Import;
-
-@Import(library = "OauthSignIn.js")
-public abstract class AbstractOauthSignIn extends OauthComponentBase {
-
-}
