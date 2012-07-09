df3332fa7d1d838ca779373288e83b34d01ec44e
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-07-09 21:17:58 +0000

    FIXED - TYNAMO-169: jpa-seedentity doesn't correctly handle SeedEntityIdentifier - use only uniquelyIdentifyingProperty as a predicate if found - add a test for entities seeded using SeedEntityIdentifier

--- tapestry-jpa-seedentity/src/test/java/org/tynamo/seedentity/jpa/services/SeedEntityImplTest.java
+++ tapestry-jpa-seedentity/src/test/java/org/tynamo/seedentity/jpa/services/SeedEntityImplTest.java
@@ -20,9 +20,11 @@ import org.apache.tapestry5.jpa.EntityManagerManager;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.tynamo.seedentity.SeedEntityIdentifier;
 import org.tynamo.seedentity.jpa.entities.AnotherThing;
 import org.tynamo.seedentity.jpa.entities.PrettyUniqueThing;
 import org.tynamo.seedentity.jpa.entities.Thing;
+import org.tynamo.seedentity.jpa.entities.ThingWithMultipleUniques;
 
 public class SeedEntityImplTest {
 	// @Inject
@@ -32,14 +34,15 @@ public class SeedEntityImplTest {
 
 	@BeforeClass
 	public void openSession() {
-		//AnnotationConfiguration configuration = new AnnotationConfiguration();
-		//configuration.addAnnotatedClass(Thing.class);
-		//configuration.configure("/hibernate-test.cfg.xml");
-		//SessionFactory sessionFactory = configuration.buildSessionFactory();
+		// AnnotationConfiguration configuration = new AnnotationConfiguration();
+		// configuration.addAnnotatedClass(Thing.class);
+		// configuration.configure("/hibernate-test.cfg.xml");
+		// SessionFactory sessionFactory = configuration.buildSessionFactory();
 		EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("testPU");
 		em = emFactory.createEntityManager();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void seedEntities() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
 		List<Object> entities = new ArrayList<Object>();
@@ -63,25 +66,33 @@ public class SeedEntityImplTest {
 		aThing.setThing(thing2);
 		entities.add(aThing);
 		entities.add(new AnotherThing());
-		
+
 		// SEE: TYNAMO-151
 		entities.add(new PrettyUniqueThing(thing, 1));
 		entities.add(new PrettyUniqueThing(thing, 1));
-		
-		Map<String,EntityManager> entityManagers = new HashMap<String,EntityManager>();
+
+		entities.add(new SeedEntityIdentifier(new ThingWithMultipleUniques("hello", "world"), "firstName"));
+		entities.add(new SeedEntityIdentifier(new ThingWithMultipleUniques("hello", "anotherWorld"), "firstName"));
+
+		Map<String, EntityManager> entityManagers = new HashMap<String, EntityManager>();
 		entityManagers.put("", em);
 		EntityManagerManager entityManagerManager = mock(EntityManagerManager.class);
 		when(entityManagerManager.getEntityManagers()).thenReturn(entityManagers);
-//		em.getTransaction().begin();
-		SeedEntity service = new SeedEntityImpl(LoggerFactory.getLogger(SeedEntity.class), propertyAccess, entityManagerManager, "", entities);
-//		em.getTransaction().commit();
+		// em.getTransaction().begin();
+		SeedEntity service = new SeedEntityImpl(LoggerFactory.getLogger(SeedEntity.class), propertyAccess,
+			entityManagerManager, "", entities);
+		// em.getTransaction().commit();
 		List<Thing> resultList = em.createQuery("select t from Thing t").getResultList();
 		assertTrue(resultList.size() == 1);
 		List<AnotherThing> aThingList = em.createQuery("select t from AnotherThing t").getResultList();
 		assertTrue(aThingList.size() == 2);
 		List<AnotherThing> aPrettyUniqueThingList = em.createQuery("select t from PrettyUniqueThing t").getResultList();
 		assertTrue(aPrettyUniqueThingList.size() == 1);
-		//assertTrue(em.createCriteria(Thing.class).list().size() > 0);
+		// assertTrue(em.createCriteria(Thing.class).list().size() > 0);
+
+		List<AnotherThing> thingWithMultipleUniquesList = em.createQuery("select t from ThingWithMultipleUniques t")
+			.getResultList();
+		assertTrue(thingWithMultipleUniquesList.size() == 1);
 	}
 
 }
