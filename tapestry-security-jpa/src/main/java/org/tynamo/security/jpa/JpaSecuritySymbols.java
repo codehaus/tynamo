1b4e2733c3abf6211d46227d64613c29cbaeee7e
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-07-15 02:59:10 +0000

    [maven-release-plugin]  copy for tag tynamo-parent-0.0.11

--- tapestry-security-jpa/src/main/java/org/tynamo/security/jpa/JpaSecuritySymbols.java
+++
@@ -1,6 +0,0 @@
-package org.tynamo.security.jpa;
-
-public class JpaSecuritySymbols {
-	public static final String ASSOCIATED_REALM = "security.associatedrealm";
-	public static final String ASSOCIATED_PRINCIPALTYPE = "security.associatedprincipaltype";
-}
