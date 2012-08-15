7e3a0b0b68efd74c9a6105c6037376abec1567f3
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-08-15 18:01:49 +0000

    [maven-release-plugin]  copy for tag tapestry-routing-0.0.4

---
+++ tags/tapestry-routing-0.0.4/src/test/java/org/tynamo/routing/services/TestsModule.java
@@ -0,0 +1,33 @@
+package org.tynamo.routing.services;
+
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.internal.InternalConstants;
+import org.apache.tapestry5.internal.InternalSymbols;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.OrderedConfiguration;
+import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.ioc.services.ApplicationDefaults;
+import org.apache.tapestry5.ioc.services.SymbolProvider;
+import org.apache.tapestry5.services.LocalizationSetter;
+import org.tynamo.routing.Route;
+
+public class TestsModule {
+
+	@Contribute(SymbolProvider.class)
+	@ApplicationDefaults
+	public static void provideApplicationDefaults(MappedConfiguration<String, String> configuration) {
+		configuration.add(InternalConstants.TAPESTRY_APP_PACKAGE_PARAM, "org.tynamo.routing");
+		configuration.add(InternalSymbols.APP_PACKAGE_PATH, "org/tynamo/routing");
+		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en_US,fi");
+	}
+
+	@Contribute(RouterDispatcher.class)
+	public static void addRoutes(OrderedConfiguration<Route> configuration, LocalizationSetter localizationSetter,
+		@Symbol(SymbolConstants.ENCODE_LOCALE_INTO_PATH) boolean encodeLocaleIntoPath) {
+		String canonicalized = "subpackage/UnannotatedPage";
+		configuration.add(canonicalized.toLowerCase(), new Route("/not/annotated/{0}", canonicalized, localizationSetter,
+			encodeLocaleIntoPath));
+	}
+	
+}
