15ec7bc1b2ea708c86c3c8894a430ad6d9e74cab
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2012-09-07 18:01:02 +0000

    OPEN - #TYNAMO-177: Django-style search/filtering based on hibernate-search - use the new, half-implemented search filter blocks

--- tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/entities/Thing.java
+++ tynamo-examples/simple/src/main/java/org/tynamo/examples/simple/entities/Thing.java
@@ -1,22 +1,15 @@
-/*
- * Copyright 2004 Chris Nelson
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License.
- */
 package org.tynamo.examples.simple.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.tynamo.PageType;
 import org.tynamo.descriptor.annotation.MethodDescriptor;
 import org.tynamo.descriptor.annotation.beaneditor.BeanModel;
 import org.tynamo.descriptor.annotation.beaneditor.BeanModels;
-
-import javax.persistence.*;
 
 @Entity
 @Table(name = "things", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
