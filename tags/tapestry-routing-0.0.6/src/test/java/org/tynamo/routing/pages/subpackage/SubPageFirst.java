adda5841f4ae3e16c992284f25e969a18f84e8dd
ascandroli <ascandroli@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2013-05-25 16:27:38 +0000

    [maven-release-plugin]  copy for tag tapestry-routing-0.0.6

---
+++ tags/tapestry-routing-0.0.6/src/test/java/org/tynamo/routing/pages/subpackage/SubPageFirst.java
@@ -0,0 +1,9 @@
+package org.tynamo.routing.pages.subpackage;
+
+
+import org.tynamo.routing.annotations.At;
+
+@At(value = "/subpackage/inventedpath", order = "before:subpackage/SubPage")
+public class SubPageFirst {
+	protected void onActivate() {}
+}
