80313eb36d988394e4492384d742d5d3292c1658
ascandroli <ascandroli@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2013-07-13 16:54:37 +0000

    [maven-release-plugin]  copy for tag tapestry-resteasy-0.3.2

---
+++ tags/tapestry-resteasy-0.3.2/src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResourceImpl.java
@@ -0,0 +1,14 @@
+package org.tynamo.resteasy.ws;
+
+import javax.ws.rs.core.Response;
+
+public class ReloadableEchoResourceImpl implements ReloadableEchoResource
+{
+
+	@Override
+	public Response echo(String message)
+	{
+		return Response.status(200).entity(message).build();
+	}
+
+}
