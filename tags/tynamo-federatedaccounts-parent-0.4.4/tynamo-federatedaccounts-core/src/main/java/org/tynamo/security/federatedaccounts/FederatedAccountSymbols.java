3b177749b629baada517e9391a2a62099b46c41f
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2013-09-10 02:34:28 +0000

    [maven-release-plugin]  copy for tag tynamo-federatedaccounts-parent-0.4.4

---
+++ tags/tynamo-federatedaccounts-parent-0.4.4/tynamo-federatedaccounts-core/src/main/java/org/tynamo/security/federatedaccounts/FederatedAccountSymbols.java
@@ -0,0 +1,15 @@
+package org.tynamo.security.federatedaccounts;
+
+public class FederatedAccountSymbols {
+	// public static final String SUCCESSURL = "federatedaccounts.successurl";
+
+	public static final String COMMITAFTER_OAUTH = "federatedaccounts.commitafteroauth";
+
+	public static final String HTTPCLIENT_ON_GAE = "httpclient.gae";
+
+	public static final String DEFAULT_RETURNPAGE = "federatedaccounts.default_returnpage";
+
+	public static final String DEFAULT_REMEMBERME = "federatedaccounts.default_rememberme";
+
+	public static final String LOCALACCOUNT_REALMNAME = "federatedaccounts.localaccount_realmname";
+}
