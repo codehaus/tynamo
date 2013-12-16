ae34f9479a579516c7ba0d2b20779b53aff9c54a
kaosko <kaosko@5a2b3bf4-0407-0410-b014-a1e54fbdd096>
2013-12-16 23:01:19 +0000

    [maven-release-plugin]  copy for tag tapestry-model-0.5.2

--- tapestry-exceptionpage/src/main/java/org/tynamo/exceptionpage/ExceptionHandlerAssistant.java
+++
@@ -1,8 +0,0 @@
-package org.tynamo.exceptionpage;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface ExceptionHandlerAssistant {
-	public Object handleRequestException(Throwable exception, List<Object> exceptionContext) throws IOException;
-}
--- tynamo-federatedaccounts/tynamo-federatedaccounts-facebook/src/main/java/org/tynamo/security/federatedaccounts/facebook/pages/FacebookSignInComponentBlocks.java
+++
@@ -1,5 +0,0 @@
-package org.tynamo.security.federatedaccounts.facebook.pages;
-
-public class FacebookSignInComponentBlocks {
-
-}
